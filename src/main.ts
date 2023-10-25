c4ed7c971ea338fd116225fd25d97ddc94e3d52e
rjsenanayaka <rjsenanayaka@gmail.com>
2023-10-25 20:12:44 +0530

    docs: added basic swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+
+  const config = new DocumentBuilder()
+    .setTitle('Gavesha WeatherKids API')
+    .setDescription('The Gavesha WeatherKids API description')
+    .setVersion('1.0')
+    .addTag('weatherkids')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
