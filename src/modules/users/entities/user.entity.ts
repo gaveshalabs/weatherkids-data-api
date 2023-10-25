ea5758ebee2c4422a95395f587abd4323b0e5230
rjsenanayaka <rjsenanayaka@gmail.com>
2023-10-25 16:39:15 +0530

    feat: added user,weather data modules with db

---
+++ src/modules/users/entities/user.entity.ts
@@ -0,0 +1,38 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { v4 as uuidv4 } from 'uuid';
+
+@Schema({
+  timestamps: true,
+  collection: 'users',
+})
+export class User {
+  @Prop({ type: String, default: uuidv4 })
+  _id: string;
+  
+  @Prop({ type: String })
+  name: string;
+
+  @Prop({ type: String, unique: true })
+  email: string;
+
+  @Prop({ type: String })
+  contact_no: string;
+
+  @Prop({ type: String })
+  nearest_city: string;
+
+  @Prop({ type: String })
+  nearest_city_postalcode: string;
+
+  @Prop({ type: String })
+  photo_url: string;
+
+  @Prop({ type: Boolean, default: true })
+  is_active: boolean;
+
+  @Prop({ type: String })
+  token: string;
+}
+
+export type UserDocument = User & Document;
+export const UserSchema = SchemaFactory.createForClass(User);
