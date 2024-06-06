e593f9fb7b8fc6ac412199a4aca1bdc09846ee57
lihini <lihini.sena@gmail.com>
2024-06-06 14:16:19 +0530

    Github workflow changes (#22)
    * feat: auth: add client validation for weather computer
    
    * fix: weather-data: include author when querying for duplicate data on commit
    
    * chore: ci authenticated through GCP WFI
    
    * fix: issues in github workflow
    
    * fix: set permissions in github workflow
    
    * fix: use node 20 following github workflow removing support for node 16
    
    * fix: debug github workflow cannot find package.json error
    
    * fix: debug github workflow cannot find package.json error
    
    * fix: debug github workflow cannot find package.json error
    
    * fix: debug github workflow cannot find package.json error
    
    * fix: use node 20 following github workflow removing support for node 16
    
    * fix: debug github workflow cannot find package.json error
    
    * fix: debug github workflow cannot find package.json error
    
    * fix: use GCP WFI instead of firebase token to deploy
    
    * fix: debug github workflow env vars not set
    
    * fix: debug github workflow use wfi instead of firebase token
    
    * fix: debug github workflow use wfi instead of firebase token
    
    * fix: debug github workflow use wfi instead of firebase token
    
    * fix: debug github workflow use wfi instead of firebase token
    
    * fix: debug github workflow use wfi instead of firebase token
    
    * chore: use service account for authenticating through wif
    
    * chore: workflow authentication with latest tools
    
    * chore: workflow authentication with latest tools
    
    * chore: workflow authentication with latest tools
    
    * chore: workflow authentication using service account key
    
    * chore: deploy debug workflow
    
    * chore: deploy workflow with env vars
    
    * chore: deploy workflow with env vars
    
    * chore: deploy workflow with env vars
    
    * chore: deploy workflow with env vars
    
    * docs: deploy workflow config
    
    * feat: accept client id and api key as body params
    
    * chore: attempt to fix error Failed to get Firebase project weatherkids-dev-67016. Please make sure the project exists and your account has permission to access it.
    
    * chore: attempt to fix error Failed to get Firebase project weatherkids-dev-67016. Please make sure the project exists and your account has permission to access it.
    
    * chore: attempt to fix error Failed to get Firebase project weatherkids-dev-67016. Please make sure the project exists and your account has permission to access it.
    
    * chore: attempt to fix error Failed to get Firebase project weatherkids-dev-67016. Please make sure the project exists and your account has permission to access it.
    
    * chore: debug error Failed to get Firebase project weatherkids-dev-67016. Please make sure the project exists and your account has permission to access it.
    
    * chore: deploy to firebase with github environment
    
    * fix: errors in points module tests

--- src/modules/points/points.controller.spec.ts
+++ src/modules/points/points.controller.spec.ts
@@ -1,6 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PointsController } from './points.controller';
 import { PointsService } from './points.service';
+import { ValidateGaveshaClientGuard } from '../common/guards/gavesha-client.guard';
+import { ValidateGaveshaUserGuard } from '../common/guards/gavesha-user.guard';
+import { TokenService } from '../users/token/token.service';
 
 describe('PointsController', () => {
   let controller: PointsController;
@@ -8,7 +11,24 @@ describe('PointsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PointsController],
-      providers: [PointsService],
+      providers: [
+        {
+          provide: PointsService,
+          useValue: () => {},
+        },
+        {
+          provide: TokenService,
+          useValue: () => {},
+        },
+        {
+          provide: ValidateGaveshaClientGuard,
+          useValue: () => {},
+        },
+        {
+          provide: ValidateGaveshaUserGuard,
+          useValue: () => {},
+        },
+      ],
     }).compile();
 
     controller = module.get<PointsController>(PointsController);
