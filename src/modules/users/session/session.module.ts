9c54e4e3be7265c4a7563d8aaf85d2713752f3fb
Hirushan99 <palihewadana11716@usci.ruh.ac.lk>
2024-07-22 19:32:30 +0530

    fix: update new_user true in session

--- src/modules/users/session/session.module.ts
+++ src/modules/users/session/session.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
-import { SessionController } from './session.controller';
-import { SessionService } from './session.service';
-import { UsersModule } from '../users.module';
+import { OAuth2Client } from 'google-auth-library';
 import { AuthModule } from 'src/modules/auth/auth.module';
 import { AuthService } from 'src/modules/auth/auth.service';
-import { OAuth2Client } from 'google-auth-library';
+import { KitePlayersModule } from 'src/modules/kite-players/kite-players.module';
+import { ClientsModule } from '../../clients/clients.module';
 import { WeatherStationsModule } from '../../weather-stations/weather-stations.module';
 import { TokenModule } from '../token/token.module';
-import { ClientsModule } from '../../clients/clients.module';
+import { UsersModule } from '../users.module';
+import { SessionController } from './session.controller';
+import { SessionService } from './session.service';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { ClientsModule } from '../../clients/clients.module';
     WeatherStationsModule,
     TokenModule,
     ClientsModule,
+    KitePlayersModule
   ],
   controllers: [SessionController],
   providers: [SessionService, OAuth2Client, AuthService],
