4f2c03f1e98dfc6955c79fdf80b531f420d64e8d
Hirushan99 <palihewadana11716@usci.ruh.ac.lk>
2024-08-14 15:52:02 +0530

    Feat: ctrate findHexagonByCoordinates method and findWeatherStationByHexagonName method

--- src/modules/weather-stations/weather-stations.module.ts
+++ src/modules/weather-stations/weather-stations.module.ts
@@ -1,15 +1,16 @@
 import { Module, forwardRef } from '@nestjs/common';
-import { WeatherStationsService } from './weather-stations.service';
-import { WeatherStationsController } from './weather-stations.controller';
 import { MongooseModule } from '@nestjs/mongoose';
+import { PointsModule } from '../points/points.module';
+import { TokenModule } from '../users/token/token.module';
+import { UsersModule } from '../users/users.module';
+import { WeatherDataModule } from '../weather-data/weather-data.module';
+import { GeoJsonHexagonSchema } from './entities/geojson-hexagon-coordinates';
 import {
   WeatherStation,
   WeatherStationSchema,
 } from './entities/weather-station.entity';
-import { UsersModule } from '../users/users.module';
-import { WeatherDataModule } from '../weather-data/weather-data.module';
-import { PointsModule } from '../points/points.module';
-import { TokenModule } from '../users/token/token.module';
+import { WeatherStationsController } from './weather-stations.controller';
+import { WeatherStationsService } from './weather-stations.service';
 
 @Module({
   imports: [
@@ -19,6 +20,7 @@ import { TokenModule } from '../users/token/token.module';
     TokenModule,
     MongooseModule.forFeature([
       { name: WeatherStation.name, schema: WeatherStationSchema },
+      {name:'GeoJsonHexaCoordinates', schema: GeoJsonHexagonSchema}
     ]),
   ],
   controllers: [WeatherStationsController],
