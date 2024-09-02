2c1b901cc4059f4e05ecbba1421a6526482c856a
Hirushan99 <palihewadana11716@usci.ruh.ac.lk>
2024-09-02 08:05:25 +0530

    Fix: Update download endpoint to generate CRC32 for file chunk

--- src/modules/downloads/downloads.service.ts
+++ src/modules/downloads/downloads.service.ts
@@ -1,4 +1,5 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
+import * as crc32 from 'crc-32';
 import { Response } from 'express';
 import * as fs from 'fs';
 import * as path from 'path';
@@ -11,31 +12,37 @@ export class DownloadsService {
       version_number: '1002004',
       filename: 'WeatherKids V4 1.2.4.bin',
       crc: '12345678',
+      file_size:'900',
     },
     {
       version_number: 'abc25d',
       filename: 'Version_1.2.3.txt',
       crc: '12345',
+      file_size:'784',
     },
     {
       version_number: '2dr',
       filename: 'Version_1.2.4.bin',
       crc: 'fgd254rg',
+      file_size:'534',
     },
     {
       version_number: '12552d',
       filename: 'Version_1.2.5.exe',
       crc: 'Defined',
+      file_size:'1024',
     },
     {
       version_number: 'hgt',
       filename: 'Version_1.2.6.txt',
       crc: 'Defined',
+      file_size:'2024',
     },
     {
       version_number: 'cvt',
       filename: 'Version_1.2.7.txt',
       crc: 'Defined',
+      file_size:'24',
     },
   ];
 
@@ -77,9 +84,10 @@ export class DownloadsService {
     }
   }
 
-  streamFile(filePath: string, res: Response, range: string | undefined): void {
+  async streamFile(filePath: string, res: Response, range: string | undefined): Promise<void> {
     const stat = fs.statSync(filePath);
     const fileSize = stat.size;
+    let crc32Value = '';
 
     if (range) {
       const parts = range.replace(/bytes=/, "").split("-");
@@ -92,28 +100,61 @@ export class DownloadsService {
       }
 
       const chunksize = (end - start) + 1;
+      crc32Value = await this.calculateCrc32ForRange(filePath, start, end);
+      
       const fileStream = fs.createReadStream(filePath, { start, end });
       const head = {
         'Content-Range': `bytes ${start}-${end}/${fileSize}`,
         'Accept-Ranges': 'bytes',
         'Content-Length': chunksize,
         'Content-Type': 'application/octet-stream',
+        'X-CRC32': `"${crc32Value}"`,
       };
 
       res.writeHead(206, head);
       fileStream.pipe(res);
     } else {
+      crc32Value = await this.calculateCrc32ForRange(filePath, 0, fileSize - 1);
+
       res.setHeader('Content-Disposition', `attachment; filename=${path.basename(filePath)}`);
       res.setHeader('Content-Type', 'application/octet-stream');
+      res.setHeader('X-CRC32', `"${crc32Value}"`);
+
       const fileStream = fs.createReadStream(filePath);
       fileStream.pipe(res);
     }
   }
 
-  findCrcByVersion(version_number: string): string {
+  findCrcByVersion(version_number: string): { file_size: string; crc: string }{
     const firmware = this.firmwareFiles.find(
       (file) => file.version_number === version_number,
     );
-    return firmware.crc;
+  
+    if (firmware) {
+      return {
+        file_size: firmware.file_size,
+        crc: firmware.crc,
+      };
+    }
   }
+
+  async  calculateCrc32ForRange(filePath: string, start: number, end: number): Promise<string> {
+    return new Promise((resolve, reject) => {
+      const fileStream = fs.createReadStream(filePath, { start, end });
+      let crc32Sum = 0;
+  
+      fileStream.on('data', (chunk) => {
+        crc32Sum = crc32.bstr(chunk.toString('binary'), crc32Sum);
+      });
+  
+      fileStream.on('end', () => {
+        const crc32Value = (crc32Sum >>> 0).toString(16).toUpperCase().padStart(8, '0');
+        resolve(crc32Value);
+      });
+  
+      fileStream.on('error', (err) => {
+        reject(err);
+      });
+    });}
+
 }
